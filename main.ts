1f089e8c4dd8ec3868901ea77733c26af6ce28b6
MuhammadHamza890 <163209886+MuhammadHamza890@users.noreply.github.com>
2024-03-19 01:31:08 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+
+const sample = await inquirer.prompt([{
+    message: "Enter your frist number",
+    type: "number",
+    name: "fristNumber"
+},
+{
+    message: "Enter your second number",
+    type: "number",
+    name: "secondNumber"
+},
+
+{
+    message: " sign operator",
+    type: "list",
+    choices: ["Addition", "Subtraction", "Multipication", "Division"],
+    name: "operator",
+},
+
+])
+if (sample.operator === "Addition") { console.log(sample.fristNumber + sample.secondNumber) }
+else if (sample.operator === "Subtraction") { console.log(sample.fristNumber - sample.secondNumber) }
+else if (sample.operator === "Multipication") { console.log(sample.fristNumber * sample.secondNumber) }
+else if (sample.operator === "Division") { console.log(sample.fristNumber / sample.secondNumber) }
+else {
+    console.log("pleses enter valid value")
+
+}
